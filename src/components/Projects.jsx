69885116bb92c775bb41ce7465a89487403abdd1
vivekmanikonda <saivivekmanikonda11@gmail.com>
2025-06-13 23:12:07 +0530

    projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ExternalLink } from 'lucide-react';
+import { title } from 'framer-motion/client';
 
 const projects = [
   {
@@ -16,21 +17,39 @@ const projects = [
     images: ["/images/p1.png", "/images/p2.png", "/images/p3.png", "/images/p4.png"],
     live: "https://matchwize.vercel.app",
   },
+
+
   {
     title: "Thrive Daily",
     description:
-      "Thrive Daily Thrive Dailyescriptions to give you a match score and actionable suggestions to improve your chances.",
-    tech: ["Next.js", "Node.js", "Express.js", "Supabase", "Tailwind CSS", "Gemini API"],
+      "ThriveDaily is a digital platform that helps users build daily habits, track personal growth, and stay motivated, promoting consistency and well-being through tools like journals and goal trackers.",
+    tech: ["TypeScript.js", "React.js", "Supabase", "Tailwind CSS", "ChatGpt API"],
     features: [
-      "Gemini API integration for resume analysis",
-      "Supabase for data storage",
-      "Gemini API integration for resume analysis",
-      "Supabase for data storage",
-      "Supabase for data storage",
+      "Track daily calorie intake and stay on top of your diet.",
+      "Log workout sessions with duration, intensity, and calories burned.",
+      "Monitor work hours and productivity for better time management.",
+      "Set personalized goals for eating, exercise, and work tasks.",
+      "View easy-to-read charts showing weekly progress and streaks.",
     ],
     images: ["/images/p1.png", "/images/p2.png", "/images/p3.png", "/images/p4.png"],
-    live: "https://matchwize.vercel.app",
+    live: "https://github.com/CodewVivek/thriveadaily",
   },
+
+
+  {
+    title: "Skypher",
+    description: "Startup Connect empowers startups to scale efficiently with a modern platform for growth. Startups can create compelling profiles, attract top talent, and grow their customer base using AI-powered insights and interactive analytics. With robust developer APIs, seamless team collaboration tools, and enterprise-grade security, it’s the ultimate hub for transforming startup visions into reality",
+    tech: ["Next.js", "Tailwind CSS", "React.js", "Supabase", "ChatGpt API"],
+    features: [
+      "Advanced Talent Attraction Tools",
+      "Enhanced Startup Profile Creation",
+      "AI-Powered Analytics",
+      "Customer Growth and Marketing Tools",
+      "Team Collaboration Tools",
+    ],
+    images: ["/images/p1.png", "/images/p2.png", "/images/p3.png", "/images/p4.png"],
+    live: "https://github.com/CodewVivek/thriveadaily",
+  }
 
 ];
 
@@ -93,7 +112,7 @@ const Projects = () => {
                   <ExternalLink size={16} /> Live
                 </a>
               </div>
-            </div>
+                          </div>
           </div>
         ))}
       </div>
